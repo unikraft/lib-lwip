5adf61d1925df62b67beca58f59ed215e4a7fa2a
Sharan Santhanam <sharan.santhanam@neclab.eu>
2018-06-15 22:53:48 +0200

    Expose socket header & disable socket polling
    In this patch, we expose the socket API through lwIP. In its current
    implementation, we do disable support for select()/poll() on a file
    descriptor since an implementation is missing in vfscore.
    We are going to add this feature again in the near future.
    
    Signed-off-by: Sharan Santhanam <sharan.santhanam@neclab.eu>

--- include/sys/socket.h
+++ include/sys/socket.h
@@ -1,7 +1,6 @@
 #ifndef UK_LWIP_SOCKET_H
 #include <uk/config.h>
 
-#ifndef CONFIG_HAVE_LIBC
 
 #if CONFIG_LIBLWIP
 #include <lwip/inet.h>
@@ -11,5 +10,4 @@
 #define SOCK_CLOEXEC    0x10000000
 #define SOCK_NONBLOCK   0x20000000
 
-#endif /* CONFIG_HAVELIBC */
 #endif /* UK_LWIP_SOCKET_H */
