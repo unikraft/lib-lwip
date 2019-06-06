393f9b6ea13c9dd7c58a30e0df46bfe04efbc169
Vlad-Andrei BĂDOIU (78692) <vlad_andrei.badoiu@stud.acs.upb.ro>
2019-06-06 16:39:03 +0000

    Add servent and protoent
    This patch adds the definition of servent and protoent to netdb.h since it is
    missing from lwip/netdb.h .
    
    Signed-off-by: Vlad-Andrei Badoiu <vlad_andrei.badoiu@stud.acs.upb.ro>
    Reviewed-by: Felipe Huici <felipe.huici@neclab.eu>

--- include/netdb.h
+++ include/netdb.h
@@ -1,1 +1,14 @@
 #include <compat/posix/netdb.h>
+
+struct servent {
+	char    *s_name;        /* official service name */
+	char    **s_aliases;    /* alias list */
+	int     s_port;         /* port # */
+	char    *s_proto;       /* protocol to use */
+};
+
+struct protoent {
+	char    *p_name;        /* official protocol name */
+	char    **p_aliases;    /* alias list */
+	int     p_proto;        /* protocol # */
+};
